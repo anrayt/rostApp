952103b992bf1993df6dc55619a7d422f1a34fd0
Anton Ratiy <anton_ratiy_tmg@whirlpool.com>
2022-09-20 20:53:37 +0300

    add grouping

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let regions = [];
+let inputData = [];
 
 fetch('input-data.json')
     .then(res => res.json())
@@ -7,6 +8,7 @@ fetch('input-data.json')
 
 
 function proceedJson(json) {
+    inputData = json;
     regions = json.map(item => item['location code'])
         .filter((value, index, self) => self.indexOf(value) === index);
     document.getElementById("available-variants").textContent = regions.join('\n');
@@ -15,9 +17,31 @@ function proceedJson(json) {
 document.getElementById("main-form").addEventListener("submit", function(e){
     let region = document.getElementById("region").value;
     if(regions.includes(region)) {
-        alert('let me check for ' + region + ' .....');
+        proceedWithRegion(region);
     } else {
         alert('You are trying to search for unsupported region');
     }
     e.preventDefault();
 });
+
+
+function proceedWithRegion(reg) {
+    let filtered = inputData.filter(v => v["location code"] === reg);
+    let map = groupBy(filtered, r => r.nighname);
+    document.getElementById("result").textContent = JSON.stringify(map, (key, value) => (value instanceof Map ? [...value] : value));
+}
+
+
+function groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+        const key = keyGetter(item);
+        const collection = map.get(key);
+        if (!collection) {
+            map.set(key, [item]);
+        } else {
+            collection.push(item);
+        }
+    });
+    return map;
+}
